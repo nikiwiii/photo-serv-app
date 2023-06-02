362953b9b215d8fa51aabfae1a8e642bbf62b57a
nikiwiii <74990279+nikiwiii@users.noreply.github.com>
2023-06-03 01:37:55 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [pictures, updatePictures] = useState([]);
+  const [ipValue, setIp] = useState('192.168.x.xx');
+
+  const getShit = async() => {
+    if (!ipValue.includes('x')){
+      let response = await fetch('http://' + ipValue + ':3001/getFiles', { method: "GET"})
+      var val = await response.json()
+      updatePictures(val.files)
+      console.log(pictures);
+    }  
+    else {
+      alert('wpisz swoje ip')
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' defaultValue={ipValue} onChange={(e) => {setIp(e.target.value)}}></input>
+      <button onClick={getShit}>Refresh/Load pictures</button>
+      <div className='file-container'>
+      {
+        pictures.map((e, i) => {
+          return(
+          <div className='Pic' key={i}>
+            <p>{e.name}</p>
+            <img src={require('./server/upload/' + e.name)}></img>
+          </div>)
+        })
+      }
+      </div>
     </div>
   );
 }
