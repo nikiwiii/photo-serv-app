4c134047fea344f6384f488f1a78a772bba7cbbb
nikiwiii <74990279+nikiwiii@users.noreply.github.com>
2023-06-04 18:42:33 +0200

    .

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 function App() {
   const [pictures, updatePictures] = useState([]);
   const [ipValue, setIp] = useState('192.168.x.xx');
+  const [selects, setSelects] = useState([]);
 
   const getShit = async() => {
     if (!ipValue.includes('x')){
@@ -16,17 +17,26 @@ function App() {
       alert('wpisz swoje ip')
     }
   }
+  const addSelect = (id) => {
+    selects.includes(id) ? setSelects(() => {return [...selects, id]}) : null//pictures[id]
+    console.log(selects);
+  }
   return (
     <div className="App">
-      <input type='text' defaultValue={ipValue} onChange={(e) => {setIp(e.target.value)}}></input>
-      <button onClick={getShit}>Refresh/Load pictures</button>
+      <div className='top'>
+        <input className='input-top' type='text' defaultValue={ipValue} onChange={(e) => {setIp(e.target.value)}}></input>
+        <button onClick={getShit} className='refresh-btn'>Refresh/Load pictures</button>
+        <input className='selects' type='text' value={'selected: ' + selects} readOnly></input>
+      </div>
       <div className='file-container'>
       {
         pictures.map((e, i) => {
           return(
-          <div className='Pic' key={i}>
-            <p>{e.name}</p>
+          <div className='Pic' key={i} onClick={() => addSelect(i)}>
+            <p><b>{i + ':'}&nbsp;</b>{e.name}</p>
             <img src={require('./server/upload/' + e.name)}></img>
+            <button>DELETE</button>
+            <button>RENAME</button>
           </div>)
         })
       }
